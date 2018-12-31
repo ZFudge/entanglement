1441534e8ee5ec24b5d1603bb97f5ed7712a62c2
Zach Fudge <zfudge@spotx.tv>
2018-12-30 19:58:22 -0700

    Added mouse movement
     Changes to be committed:
    	modified:   entanglement.js

--- entanglement.js
+++ entanglement.js
@@ -4,14 +4,38 @@ function Dot(x, y, radius, verticalVelocity, horizontalVelocity) {
     this.radius = radius;
     this.verticalVelocity = verticalVelocity;
     this.horizontalVelocity = horizontalVelocity;
+    this.baseVerticalVelocity = verticalVelocity;
+    this.baseHorizontalVelocity = horizontalVelocity;
 }
 
 Dot.prototype.adjust = function(index) {
-    this.y += this.verticalVelocity;
-    this.x += this.horizontalVelocity;
+    const outerRadius = this.radius * dot.outerRadiusMultiplier;
+    if (mouse.coordinates.x && mouse.proximityCheck(this.x, this.y, outerRadius)) {
+        let x;
+        let y;
+        [x, y] = mouse.calculateDrift(this.x, this.y);
+        this.x += x;
+        this.y += y;
+        this.horizontalVelocity = x;
+        this.verticalVelocity = y;
+        this.baseHorizontalVelocity = x / mouse.intensityLimit;
+        this.baseVerticalVelocity = y / mouse.intensityLimit;
+    } else if (Math.abs(this.horizontalVelocity) > Math.abs(this.baseHorizontalVelocity) && Math.abs(this.horizontalVelocity - this.baseHorizontalVelocity) > 0.1) {
+        this.horizontalVelocity *= 0.99;
+        this.verticalVelocity *= 0.99;
+        this.x += this.horizontalVelocity;
+        this.y += this.verticalVelocity;
+    } else {
+        this.x += this.horizontalVelocity;
+        this.y += this.verticalVelocity;
+    }
     const remove = this.removeCheck();
-    (remove) ? this.remove() : this.draw();
-    dot.entangleCheck(index);
+    if (remove) {
+        this.remove();
+    } else {
+        this.draw();
+        if (dot.dots.length > 0) dot.entangleAndMergeCheck(index);
+    }
 }
 
 Dot.prototype.draw = function() {
@@ -35,7 +59,7 @@ Dot.prototype.remove = function() {
 }
 
 const dot = {
-    ms: 100,
+    ms: 50,
     maxBlurWidth: 3,
     outerRadiusMultiplier: 60,
     dots: [],
@@ -56,14 +80,19 @@ const dot = {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.dots.reverse().forEach((cur, index) => cur.adjust(index));
     },
-    entangleCheck(index) {
+    merge() {
+
+    },
+    entangleAndMergeCheck(index) {
         if (index > 0) {
             const currentDot = this.dots[index];
             for (let i = index - 1; i >= 0; i--) {
                 const nextDot = this.dots[i];
                 const outerRadius = (currentDot.radius > nextDot.radius) ? currentDot.radius * this.outerRadiusMultiplier : nextDot.radius * this.outerRadiusMultiplier;
                 const hypotenuse = (((currentDot.x - nextDot.x) ** 2) + ((currentDot.y - nextDot.y) ** 2)) ** 0.5;
-                if (hypotenuse < outerRadius) {
+                if (hypotenuse < currentDot.radius + nextDot.radius) {
+                    this.merge(currentDot, nextDot, outerRadius, hypotenuse);
+                } else if (hypotenuse < outerRadius) {
                     this.entangle(currentDot, nextDot, outerRadius, hypotenuse);
                 }
             }
@@ -73,7 +102,7 @@ const dot = {
         const intensity = outerRadius - distance;
         const width = (1 / outerRadius) * intensity * this.maxBlurWidth;
         const ctx = this.context;
-        ctx.shadowBlur = width 100;
+        ctx.shadowBlur = width * 100;
         ctx.lineWidth = width;
         ctx.beginPath();
         ctx.moveTo(dot1.x, dot1.y);
@@ -85,18 +114,11 @@ const dot = {
         const ctx = this.context;
         cnv.width = width;
         cnv.height = height;
+        ctx.lineCap = 'round';
         ctx.shadowColor = 'white';
         ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'white';
         ctx.shadowBlur = 0.5;
-    },
-    mouseMove(x, y) {
-        //console.log(arguments)
-    },
-    click(x, y) {
-        const radius = this.randomRadius();
-        const verticalVelocity = this.randomVelocity();
-        const horizontalVelocity = this.randomVelocity();
-        this.dots.push(new Dot(x, y, radius, verticalVelocity, horizontalVelocity));
     },
     minRadius: 1,
     radiusRange: 2,
@@ -104,13 +126,65 @@ const dot = {
         return Math.ceil(this.minRadius + Math.random() * this.radiusRange);
     },
     randomVelocity() {
-        return Math.random() - 0.5;
+        return (Math.random() - 0.5) / 2;
     }
 };
+
+const mouse = {
+    click(x, y) {
+        const radius = dot.randomRadius();
+        const verticalVelocity = dot.randomVelocity();
+        const horizontalVelocity = dot.randomVelocity();
+        dot.dots.push(new Dot(x, y, radius, verticalVelocity, horizontalVelocity));
+    },
+    move(x, y) {
+        this.coordinates.x = x;
+        this.coordinates.y = y;
+    },
+    coordinates: {
+        reset() {
+            delete this.x;
+            delete this.y;
+        }
+    },
+    proximityCheck(x, y, outerRadius) {
+        const hypotenuse = (((this.coordinates.x - x) ** 2) + ((this.coordinates.y - y) ** 2)) ** 0.5;
+        return (hypotenuse < outerRadius);
+    },
+    intensityLimit: 2,
+    function logslider(position) {
+        // position will be between 0 and 100
+        var minp = 0;
+        var maxp = 100;
+
+        // The result should be between 100 an 10000000
+        var minv = Math.log(100);
+        var maxv = Math.log(10000000);
+
+        // calculate adjustment factor
+        var scale = (maxv-minv) / (maxp-minp);
+
+        return Math.exp(minv + scale*(position-minp));
+    },
+    calculateDrift(x2, y2, outerRadius) {
+
+        const x1 = this.coordinates.x;
+        const y1 = this.coordinates.y;
+        const xDiff = x1 - x2;
+        const yDiff = y1 - y2;
+        const hypotenuse = ((xDiff ** 2) + (yDiff ** 2)) ** 0.5;
+        const intensity = this.intensityLimit / (outerRadius - hypotenuse);
+        const unit = -this.intensityLimit / (Math.abs(xDiff) + Math.abs(yDiff));
+        let x = unit * xDiff;
+        let y = unit * yDiff;
+        return [x, y];
+    }
+}
 
 function main() {
     dot.dripCheck();
     dot.adjust();
+    mouse.coordinates.reset();
 }
 
 dot.canvas = document.getElementById('entanglement-canvas');
@@ -123,11 +197,11 @@ dot.context.strokeStyle = 'white';
 dot.loop = setInterval(main, dot.ms);
 
 window.addEventListener("resize", () => dot.resize(innerWidth, innerHeight));
-document.addEventListener('mousemove', (e) => dot.mouseMove(e.clientX, e.clientY));
-document.addEventListener('click', (e) => dot.click(e.clientX, e.clientY));
+document.addEventListener('mousemove', (e) => mouse.move(e.clientX, e.clientY));
+document.addEventListener('click', (e) => mouse.click(e.clientX, e.clientY));
 
 // must occur after initial resize
-(function(total) {
+(function(total = 0) {
     for (var i = 0; i < total; i++) {
         const x = Math.floor(Math.random() * dot.canvas.width * 0.8);
         const y = Math.floor(Math.random() * dot.canvas.height * 0.8);
@@ -136,4 +210,4 @@ document.addEventListener('click', (e) => dot.click(e.clientX, e.clientY));
         const horizontalVelocity = dot.randomVelocity();
         dot.dots.push(new Dot(x, y, radius, verticalVelocity, horizontalVelocity));
     }
-})(10);
+})();
